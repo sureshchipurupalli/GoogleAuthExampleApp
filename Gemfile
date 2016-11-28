2f1790448e0f954fdadc6fba5c5cfade9671c637
suresh.ch471 <suresh.ch471@gmail.com>
2016-11-28 11:03:05 +0000

    dsf

--- Gemfile
+++ Gemfile
@@ -2,8 +2,10 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
+gem 'omniauth-oauth2', '~> 1.3.1'
 
 gem 'omniauth-google-oauth2'
+
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
-OmniAuth.config.logger = Rails.logger
+Rails.application.config.x.settings["oauth2"] = {} if Rails.env.test?
 
+# [START omniauth_configuration]
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, 'my google client id', 'my google client secret', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  config = Rails.application.config.x.settings["oauth2"]
+
+  provider :google_oauth2, "115059498414-5fj2in117vueg4a1tdg52f61d0k07hnp.apps.googleusercontent.com",
+                           "ZbPzUjSKpYRTmGXW2uqhIRfc",
+                           image_size: 150
 end
